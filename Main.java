7b466efe90059ee602dfdf96d0a23de86520ac90
AustinKavelman <kavea9410@wrdsb.ca>
2020-09-17 22:10:55 +0000

    Finished the program that determines if a number is even/odd.

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * THis program determines if the number is true or false
+ * @author Austin Kavelman
  */
 public class Main {
 
@@ -9,7 +11,17 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //Gets the users number and prints it on the screen
+    System.out.println("Please enter an interger:");
+    int number = input.nextInt();
+
+    //Checks to see if number is odd/even
+    if ( number % 2 == 0 )
+        System.out.println("Entered number is even");
+     else
+        System.out.println("Entered number is odd");
   }
 }
